ec73c4e9222d629968fc8f621bf130998f436804
socloccoc <daihusk57@gmail.com>
2020-03-12 12:18:43 +0700

    fix

--- Api/app/Console/Commands/ScheduleCheckDe.php
+++ Api/app/Console/Commands/ScheduleCheckDe.php
@@ -9,7 +9,8 @@ use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
-class ScheduleCheckDe extends Command {
+class ScheduleCheckDe extends Command
+{
     /**
      * The name and signature of the console command.
      *
@@ -29,7 +30,8 @@ class ScheduleCheckDe extends Command {
      *
      * @return void
      */
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
@@ -38,9 +40,10 @@ class ScheduleCheckDe extends Command {
      *
      * @return mixed
      */
-    public function handle() {
+    public function handle()
+    {
         $currentDate = Carbon::now()->format('d-m-Y');
-        $daily       = Daily::where('date', $currentDate)->first();
+        $daily = Daily::where('date', $currentDate)->first();
         if (empty($daily)) {
             $this->info('Daily không tồn tại !');
             return false;
@@ -60,41 +63,56 @@ class ScheduleCheckDe extends Command {
             ->groupBy('points.num')
             ->orderBy('sum', 'desc')
             ->get();
+
         $bacangs = Point::whereIn('customer_daily_id', $listCustomerDaily)
             ->where('type', 4)
             ->selectRaw('points.*, sum(diem_tien) as sum')
             ->groupBy('points.num')
             ->orderBy('sum', 'desc')
             ->get();
-        $deMsg          = "<b>De. </b> \n";
-        $bacangMsg      = "<b>Bacang. </b> \n";
-        $deRecomMsg     = "<b>De. </b> \n";
-        $bacangRecomMsg = "<b>Bacang. </b> \n";
-        foreach ($des as $point) {
-            if ($point['sum'] >= 200000) {
-                $deMsg .= $point['num'] . 'x' . $point['sum'] / 1000 . 'n.' . "\n";
-                if ($point['sum'] > 200000) {
-                    $deRecomMsg .= $point['num'] . 'x' . ($point['sum'] - 200000) / 1000 . 'n.' . "\n";
-                }
-            }
-        }
 
-        foreach ($bacangs as $point) {
-            if ($point['sum'] >= 20000) {
-                $bacangMsg .= $point['num'] . 'x' . $point['sum'] / 1000 . 'n.' . "\n";
-                if ($point['sum'] > 20000) {
-                    $bacangRecomMsg .= $point['num'] . 'x' . ($point['sum'] - 20000) / 1000 . 'n.' . "\n";
-                }
-            }
-        }
+        $deMsg = "<b>De. </b> \n";
+        $bacangMsg = "<b>Bacang. </b> \n";
+        $deRecomMsg = "<b>De. </b> \n";
+        $bacangRecomMsg = "<b>Bacang. </b> \n";
+
+//        foreach ($des as $point) {
+//            if ($point['sum'] >= 200000) {
+//                $deMsg .= $point['num'] . 'x' . $point['sum'] / 1000 . 'n.' . "\n";
+//                if ($point['sum'] > 200000) {
+//                    $deRecomMsg .= $point['num'] . 'x' . ($point['sum'] - 200000) / 1000 . 'n.' . "\n";
+//                }
+//            }
+//        }
+//
+//
+//        foreach ($bacangs as $point) {
+//            if ($point['sum'] >= 20000) {
+//                $bacangMsg .= $point['num'] . 'x' . $point['sum'] / 1000 . 'n.' . "\n";
+//                if ($point['sum'] > 20000) {
+//                    $bacangRecomMsg .= $point['num'] . 'x' . ($point['sum'] - 20000) / 1000 . 'n.' . "\n";
+//                }
+//            }
+//        }
+//
+//        $text = "<b>Thông tin bộ số lớn ngày " . $currentDate . "</b>\n"
+//            . (strlen($deMsg) > 15 ? $deMsg : '')
+//            . (strlen($bacangMsg) > 20 ? $bacangMsg : '');
+//
+//        $textRecom = "<b>Khuyến nghị " . $currentDate . "</b>\n"
+//            . (strlen($deRecomMsg) > 15 ? $deRecomMsg : '')
+//            . (strlen($bacangRecomMsg) > 20 ? $bacangRecomMsg : '');
+
+        $de = $this->getMsg($des, 200000, $deMsg, $deRecomMsg);
+        $bacang = $this->getMsg($bacangs, 20000, $bacangMsg, $bacangRecomMsg);
 
         $text = "<b>Thông tin bộ số lớn ngày " . $currentDate . "</b>\n"
-        . (strlen($deMsg) > 15 ? $deMsg : '')
-        . (strlen($bacangMsg) > 20 ? $bacangMsg : '');
+            . (strlen($de[0]) > 15 ? $de[0] : '')
+            . (strlen($bacang[0]) > 20 ? $bacang[0] : '');
 
         $textRecom = "<b>Khuyến nghị " . $currentDate . "</b>\n"
-        . (strlen($deRecomMsg) > 15 ? $deRecomMsg : '')
-        . (strlen($bacangRecomMsg) > 20 ? $bacangRecomMsg : '');
+            . (strlen($de[1]) > 15 ? $de[1] : '')
+            . (strlen($bacang[1]) > 20 ? $bacang[1] : '');
 
         Telegram::sendMessage([
             'chat_id'    => config('constants.CHANNEL_ID'),
@@ -107,5 +125,37 @@ class ScheduleCheckDe extends Command {
             'parse_mode' => 'HTML',
             'text'       => $textRecom,
         ]);
+    }
+
+    public function getMsg($data, $cross, $msg1, $msg2)
+    {
+        $arrs = [];
+        if (!empty($data)) {
+            for ($i = 0 ; $i < count($data) ; $i++) {
+                $nums[] = $data[$i]['num'];
+                if ($i < count($data) - 1) {
+                    if ($data[$i]['sum'] != $data[$i + 1]['sum']) {
+                        $arrs[$data[$i]['sum']] = $nums;
+                        $nums = [];
+                    }
+                } else {
+                    if ($data[$i]['sum'] == $data[$i - 1]['sum']) {
+                        $arrs[$data[$i - 1]['sum']][] = $data[$i]['num'];
+                    } else {
+                        $arrs[$data[$i]['sum']] = $data[$i]['num'];
+                    }
+                }
+            }
+            foreach ($arrs as $ind => $arr) {
+                if ($ind >= $cross) {
+                    $msg1 .= implode(',', $arr) . 'x' . $ind / 1000 . 'n.' . "\n";
+                    if ($ind > $cross) {
+                        $msg2 .= implode(',', $arr) . 'x' . ($ind - $cross) / 1000 . 'n.' . "\n";
+                    }
+                }
+            }
+        }
+
+        return [$msg1, $msg2];
     }
 }
--- Api/app/Console/Commands/ScheduleCheckLo.php
+++ Api/app/Console/Commands/ScheduleCheckLo.php
@@ -71,42 +71,87 @@ class ScheduleCheckLo extends Command {
         $xienMsg      = "<b>Xien.</b> \n";
         $loRecomMsg   = "<b>Lo.</b> \n";
         $xienRecomMsg = "<b>Xien.</b> \n";
-        foreach ($los as $point) {
-            if ($point['sum'] >= 250) {
-                $loMsg .= $point['num'] . 'x' . $point['sum'] . 'đ.' . "\n";
-                if ($point['sum'] > 250) {
-                    $loRecomMsg .= $point['num'] . 'x' . $point['sum'] - 250 . 'đ.' . "\n";
-                }
-            }
-        }
+//        foreach ($los as $point) {
+//            if ($point['sum'] >= 250) {
+//                $loMsg .= $point['num'] . 'x' . $point['sum'] . 'đ.' . "\n";
+//                if ($point['sum'] > 250) {
+//                    $loRecomMsg .= $point['num'] . 'x' . $point['sum'] - 250 . 'đ.' . "\n";
+//                }
+//            }
+//        }
+//
+//        foreach ($xiens as $point) {
+//            if ($point['sum'] >= 300000) {
+//                $xienMsg .= $point['num'] . 'x' . $point['sum'] / 1000 . 'n.' . "\n";
+//                if ($point['sum'] > 300000) {
+//                    $xienRecomMsg .= $point['num'] . 'x' . ($point['sum'] - 300000) / 1000 . 'n.' . "\n";
+//                }
+//            }
+//        }
+//
+//        $text = "<b>Thông tin bộ số lớn ngày " . $currentDate . "</b>\n"
+//        . (strlen($loMsg) > 15 ? $loMsg : '')
+//        . (strlen($xienMsg) > 15 ? $xienMsg : '');
+//
+//        $textRecom = "<b>Khuyến nghị " . $currentDate . "</b>\n"
+//        . (strlen($loRecomMsg) > 15 ? $loRecomMsg : '')
+//        . (strlen($xienRecomMsg) > 15 ? $xienRecomMsg : '');
 
-        foreach ($xiens as $point) {
-            if ($point['sum'] >= 300000) {
-                $xienMsg .= $point['num'] . 'x' . $point['sum'] / 1000 . 'n.' . "\n";
-                if ($point['sum'] > 300000) {
-                    $xienRecomMsg .= $point['num'] . 'x' . ($point['sum'] - 300000) / 1000 . 'n.' . "\n";
-                }
-            }
-        }
+        $lo = $this->getMsg($los, 250, $loMsg, $loRecomMsg, true);
+        $xien = $this->getMsg($xiens, 300000, $xienMsg, $xienRecomMsg);
 
         $text = "<b>Thông tin bộ số lớn ngày " . $currentDate . "</b>\n"
-        . (strlen($loMsg) > 15 ? $loMsg : '')
-        . (strlen($xienMsg) > 15 ? $xienMsg : '');
+        . (strlen($lo[0]) > 15 ? $lo[0] : '')
+        . (strlen($xien[0]) > 15 ? $xien[0] : '');
 
         $textRecom = "<b>Khuyến nghị " . $currentDate . "</b>\n"
-        . (strlen($loRecomMsg) > 15 ? $loRecomMsg : '')
-        . (strlen($xienRecomMsg) > 15 ? $xienRecomMsg : '');
+        . (strlen($lo[1]) > 15 ? $lo[1] : '')
+        . (strlen($xien[1]) > 15 ? $xien[1] : '');
 
         Telegram::sendMessage([
             'chat_id'    => config('constants.CHANNEL_ID'),
             'parse_mode' => 'HTML',
             'text'       => $text,
         ]);
+
         Telegram::sendMessage([
             'chat_id'    => config('constants.CHANNEL_ID'),
             'parse_mode' => 'HTML',
             'text'       => $textRecom,
         ]);
 
+    }
+
+    public function getMsg($data, $cross, $msg1, $msg2, $islo = false)
+    {
+        $arrs = [];
+        if (!empty($data)) {
+            for ($i = 0 ; $i < count($data) ; $i++) {
+                $nums[] = $data[$i]['num'];
+                if ($i < count($data) - 1) {
+                    if ($data[$i]['sum'] != $data[$i + 1]['sum']) {
+                        $arrs[$data[$i]['sum']] = $nums;
+                        $nums = [];
+                    }
+                } else {
+                    if ($data[$i]['sum'] == $data[$i - 1]['sum']) {
+                        $arrs[$data[$i - 1]['sum']][] = $data[$i]['num'];
+                    } else {
+                        $arrs[$data[$i]['sum']] = $data[$i]['num'];
+                    }
+                }
+            }
+            foreach ($arrs as $ind => $arr) {
+                if ($ind >= $cross) {
+                    $divisor = $islo ? 1 : 1000;
+                    $msg1 .= implode(',', $arr) . 'x' . $ind / $divisor . 'n.' . "\n";
+                    if ($ind > $cross) {
+                        $msg2 .= implode(',', $arr) . 'x' . ($ind - $cross) / $divisor . 'n.' . "\n";
+                    }
+                }
+            }
+        }
+
+        return [$msg1, $msg2];
     }
 }
