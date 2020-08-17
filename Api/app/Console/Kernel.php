e4504ead42b628e87ab50a6ba5b91c76047157f2
socloccoc <daihusk57@gmail.com>
2020-08-17 18:50:19 +0700

    change kernel

--- Api/app/Console/Kernel.php
+++ Api/app/Console/Kernel.php
@@ -23,15 +23,15 @@ class Kernel extends ConsoleKernel {
      */
     protected function schedule(Schedule $schedule) {
         $schedule->command('calculate:start')
-            ->dailyAt('18:35')->appendOutputTo(storage_path('logs/calculate.log'));
+            ->dailyAt('18:40')->appendOutputTo(storage_path('logs/calculate.log'));
         $schedule->command('daily:start')
-            ->dailyAt('18:40')->appendOutputTo(storage_path('logs/daily.log'));
+            ->dailyAt('18:45')->appendOutputTo(storage_path('logs/daily.log'));
         $schedule->command('checkLo:start')
             ->dailyAt('18:08')->appendOutputTo(storage_path('logs/checkLo.log'));
         $schedule->command('checkDe:start')
             ->dailyAt('18:18')->appendOutputTo(storage_path('logs/checkDe.log'));
         $schedule->command('checkResult:start')
-            ->dailyAt('18:36')->appendOutputTo(storage_path('logs/checkResult.log'));
+            ->dailyAt('18:41')->appendOutputTo(storage_path('logs/checkResult.log'));
     }
 
     /**
