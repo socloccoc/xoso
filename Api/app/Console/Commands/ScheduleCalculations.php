bbb726e8ac173c115f2e242431262867e34bfc1e
socloccoc <daihusk57@gmail.com>
2021-11-16 12:31:50 +0700

    f

--- Api/app/Console/Commands/ScheduleCalculations.php
+++ Api/app/Console/Commands/ScheduleCalculations.php
@@ -106,7 +106,7 @@ class ScheduleCalculations extends Command
 
     public function ticketHandle($result, $baCang)
     {
-        $currentDate = Carbon::now()->subDay()->format('d-m-Y');
+        $currentDate = Carbon::now()->format('d-m-Y');
         $daily = Daily::where('date', $currentDate)->first();
         if (empty($daily)) {
             $this->info('Daily không tồn tại !');
@@ -229,7 +229,7 @@ class ScheduleCalculations extends Command
     }
 
     public function updateResultDaily($result){
-        $currentDate = Carbon::now()->subDay()->format('d-m-Y');
+        $currentDate = Carbon::now()->format('d-m-Y');
         $daily = Daily::where('date', $currentDate)->first();
         if (empty($daily)) {
             $this->info('Không tìm thấy daily !');
@@ -240,7 +240,7 @@ class ScheduleCalculations extends Command
 
     public function syntheticTicket()
     {
-        $currentDate = Carbon::now()->subDay()->format('d-m-Y');
+        $currentDate = Carbon::now()->format('d-m-Y');
         $daily = Daily::where('date', $currentDate)->first();
         if (empty($daily)) {
             $this->info('Không tìm thấy daily !');
